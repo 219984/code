787a9fc4d8f63f9e5d6b2438fdffbd7da391fc59
219984 <1801777893@qq.com>
2025-10-04 23:14:11 +0800

    第一次改

---
+++ test2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+//     int a=40;
+//     int c = 212;
+//     int d = (-8+22)*a -10+c/2;
+//     printf ("%d\n",d);
+//  int arr[]={ };
+char arr[4] = {'b','i','t',};
+printf ("%d\n",strlen(arr));
+    
+    return 0;
+}
