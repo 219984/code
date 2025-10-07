82dab6f6dfd2b528a4ac6702f50b5249e0d84c17
219984 <1801777893@qq.com>
2025-10-07 22:04:39 +0800

    2

---
+++ test6.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+// struct stu 
+// {
+//     char name[20];
+//     char sex[10];
+//     int age;
+//     char tele[12];
+// } ;
+// void print(struct stu* ps )
+// {
+//     printf("%s %s %d %s\n",ps->name,ps->sex,ps->age,ps->tele);
+// }
+// int main ()
+// {
+//     struct stu s = {"zhamgsna","nan",20,"12345678123"};
+//     print(&s); 
+//     return 0;
+// }
+// int main ()
+// {
+//     int a = 0;
+//     int b = 2;
+//     if (a == 1)
+//     {
+//         if (b == 2)
+//             printf("hehe\n");
+//         else
+//             printf("haha\n");
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     int i = 0;
+//     while(i<=100) 
+//     {
+//         if (i%2 == 1)
+//         printf (" %d", i );
+//     }
+//     return 0;
+//     }
+    
+// int main ()
+// {
+//     int day = 0;
+//     scanf("%d",&day);
+//     switch (day)
+//     {
+//         case 1:
+//         printf("星期一\n");
+//         break; 
+//         default:
+//         printf("输入错误\n");
+//         break;
+
+//     }
+//     return 0;
+// }
+int main ()
+{
+    int ch = 0;
+    while((ch = getchar()) != EOF)
+    {
+        putchar(ch);
+    }
+    return 0;
+}
