98bf5d764c3a124b02d0c7594aecd1efc4dac4d0
219984 <1801777893@qq.com>
2025-10-12 23:21:55 +0800

    2025_10_12

--- test.c
+++ test.c
@@ -1,8 +1,23 @@
 #include<stdio.h>
-
-int main ()
+int max(int a,int b)
 {
-    printf("hallo. world!");
-    return 0;
-    
+    int z= 0;
+    if(a>b)
+    {
+        z = a;
+    }
+    else
+    {
+        z = b;
+    }
+    return z;
+}
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    scanf("%d %d",&a,&b);
+    c = max(a,b);
+    printf ("%d\n",c);
 }
