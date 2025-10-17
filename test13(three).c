96e7f2cc8b2ad7210212d202f701a7cf7b69aa8c
219984 <1801777893@qq.com>
2025-10-17 23:01:27 +0800

    2025_10_17

--- test13(three).c
+++ test13(three).c
@@ -3,6 +3,17 @@
 #define COL 3
 void initboard(char board[ROW][COL], int row, int col);
 void displayboard(char board[ROW][COL],int row,int col);
+void PlayerMove(char board[ROW][COL],int row, int col);
+void ComputerMove(char board[ROW][COL], int row, int col);
+//
+//玩家赢-'*'
+//电脑赢-'#'
+//平局-'Q'
+//继续-'C'
+char IsWin(char board[ROW][COL],int row,int col);
+
+
+
 
 //初始化棋盘
 void initboard(char board[ROW][COL], int row, int col)
@@ -46,36 +57,142 @@ void displayboard(char board[ROW][COL], int row, int col)
 
 }
 
+//打印棋盘
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0;i < row;i++)
+	{
+		int j = 0;
+		for (j = 0;j < col;j++)
+		{
+			printf(" %c ",board[i][j]);
+			if(j<col-1)
+			printf("|");
+		}
+		printf("\n");
+		if (i < row - 1)
+		{
+			for (j = 0;j < col;j++)
+			{
+					printf("---");
+					if (j < col - 1)
+						printf("|");
+			}
+			printf("\n");
+		}
+	}
+}
+
+//玩家移动
+void PlayerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("玩家下棋:>\n");
+	while (1)
+	{
+		printf("请输入坐标:>\n");
+		scanf("%d %d", &x, &y);
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+				printf("此位置已经有棋，请重下\n");
+		}
+		else
+		{
+			printf("输入错误，重新输入\n");
+		}
+	}
+}
+
+//电脑下棋
+void ComputerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	while (1)
+	{
+		x = rand() % row;
+		y = rand() % col;
+		if (board[x][y] == ' ')
+		{
+			board[x][y] = '#';
+			break;
+		}
+	}
+}
 
 
+//判断输赢
+char IsWin(char board[ROW][COL], int row, int col)
+{
+
+}
 
 
 void game()
 {
-	char board[ROW][COL] = { 0 };
+	char ret = 0;
+	//生成数组做棋盘
+	char board[ROW][COL];
 	//初始化棋盘
-	initboard(board, ROW, COL);
-	displayboard(board, ROW, COL);
+	InitBoard(board,ROW,COL);
+	//打印棋盘
+	DisplayBoard(board,ROW,COL);
+	
+	while (1)//玩家下棋
+	{
+		PlayerMove(board, ROW, COL);
+		//判断输赢
+		ret = IsWin(board,ROW,COL);
+		if (ret != 'c')
+		{
+			break;
+		}
+		DisplayBoard(board, ROW, COL);
+		ComputerMove(board, ROW, COL);
+		//判断输赢
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'c')
+		{
+			break;
+		}
+		DisplayBoard(board, ROW, COL);
+	}
+	if (ret == '*')
+	{
+		printf("玩家赢了\n");
+	}
+	else if (ret == '#')
+	{
+		printf("电脑赢了\n");
+	}
+	else
+	{
+		printf("平局了\n");
+	}
 }
-void menu()
-{
-	printf("*********************\n");
-	printf("*** 1.play 0.exit ***\n");
-	printf("*********************\n");
 
-}
 int main()
 {
+	srand((unsigned int)time(NULL));//设置随机数生成起点
 	int input = 0;
+
 	do
 	{
-		menu();//打印菜单
+		menu();
 		printf("请选择:>");
 		scanf("%d", &input);
 		switch (input)
 		{
 		case 1:
-			printf("开始游戏\n");
+			printf("三子棋\n");
 			game();
 			break;
 		case 0:
@@ -84,7 +201,10 @@ int main()
 		default:
 			printf("输入错误\n");
 			break;
+
 		}
 	} while (input);
+
 	return 0;
+
 }
