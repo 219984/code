a746c2be56421ac97f872d43e6401e2f61dcbe4e
219984 <1801777893@qq.com>
2025-10-22 23:26:48 +0800

    2025_10_22

--- test17.c
+++ test17.c
@@ -22,12 +22,104 @@
 // }
 
 
+// int main()
+// {
+//     int a = 0;
+//     int b = 0;
+//     scanf ("%d %d",&a,&b);
+//     int max =a>b?a:b;
+//     printf ("%d\n",max);
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     char a = 0xb6;
+//     //if(a==0xb6)
+//     printf ("%d\n",a);
+//     return 0;
+// }
+
+//小乐乐走台阶
+// int fib (int n)
+// {
+//     if (n<=2)
+//     {
+//         return n;
+//     }
+//     else 
+//     {
+//         return fib(n-1)+fib(n-2);
+//     }
+// }
+// int main ()
+// {
+//     int n = 0;
+//     scanf ("%d",&n);
+//     int m = fib(n);
+//     printf ("%d\n",m);
+//     return 0;
+// }
+
+// int main()
+// {
+//     int arr[] = {4, 3, 2, 5, 8, 7, 2, 0, 9};
+//     int i = 0;
+//     int j = 0;
+//     int del = 0;
+//     int count = 0;
+//     scanf("%d", &del);
+//     for (i = 0; i < 9; i++)
+//     {
+//         if (arr[i] != del)
+//         {
+//             arr[j++] = arr[i];
+//         }
+//         else
+//             count++;
+//     }
+//     for (i = 0; i < 9 - count; i++)
+//         printf("%d ", arr[i]);
+//     return 0;
+// }
+
+// int main()
+// {
+//      int arr[] = {4, 3, 2, 5, 8, 7, 2, 0, 9};
+
+//     return 0;
+// }
+
+
 int main()
 {
-    int a = 0;
-    int b = 0;
-    scanf ("%d %d",&a,&b);
-    int max =a>b?a:b;
-    printf ("%d\n",max);
+    int n = 0;
+    scanf ("%d",&n);
+    int arr[10]={0};
+    int i = 0;
+    for (i=0;i<10;i++)
+    {
+        scanf ("%d",&arr[i]);
+    }
+    int sz = sizeof(arr)/sizeof(arr[0]);
+    for(i=0;i<sz;i++)
+    {
+        int j = 0;
+        for (j=0;j<sz-1-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int tmp = 0;
+                tmp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=tmp;
+            }
+        }
+    }
+    printf ("%d\n",arr[0]);
+    printf ("%d\n",arr[sz-1]);
+
+
     return 0;
 }
