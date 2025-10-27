5f1a683360cc63202bcbb0c7101834ec3fc68d87
219984 <1801777893@qq.com>
2025-10-27 22:58:07 +0800

    2025_10_27

--- test19.c
+++ test19.c
@@ -49,21 +49,54 @@
 // }
 
 //高手写法
-int count_1(int n)
+// int count_1(int n)
+// {
+//     int count = 0;
+//     while(n)
+//     {
+//         count ++;
+//         n = n & (n-1);
+//     }
+//     return count;
+// }
+// int main ()
+// {
+//     int n = 0;
+//     scanf("%d",&n);
+//     int num = count_1(n);
+//     printf ("%d\n",num);
+//     return 0;
+// }
+
+//m,n二进制位不同的个数
+// int main ()
+// {
+//     int m = 0;
+//     int n = 0;
+//     scanf ("%d %d",&m,&n);
+//     int i = 0;
+//     int count = 0;
+//     for (i = 0;i<32;i++)
+//     {
+//         if(((m>>i)&1)!=((n>>i)&1))
+//         {
+//             count ++;
+//         }
+//     }
+//     printf("%d\n",count);
+//     return 0;
+// }
+
+//交换两个变量的内容
+int main()
 {
-    int count = 0;
-    while(n)
-    {
-        count ++;
-        n = n & (n-1);
-    }
-    return count;
-}
-int main ()
-{
-    int n = 0;
-    scanf("%d",&n);
-    int num = count_1(n);
-    printf ("%d\n",num);
+    int a = 1;
+    int b = 2;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    printf("%d ", a);
+    printf("%d ", b);
+
     return 0;
 }
