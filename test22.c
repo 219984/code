36ffe64f95616893d72b08ab9be224dd86d7bfbc
219984 <1801777893@qq.com>
2025-11-01 21:49:32 +0800

    2025_11_1

---
+++ test22.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char* my_strcpy(char*dist,char* star)
+{
+    char* ret = dist;
+    while(*dist++=*star++)
+
+    {
+        ;
+    }
+    return ret;
+}
+int main ()
+{
+    char arr1[20]={0};
+    char arr2[]="hell0 world";
+    printf("%s\n",my_strcpy(arr1,arr2));
+    return 0;
+}
